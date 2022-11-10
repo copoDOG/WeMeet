3622d67675add2091fa9a121b17d193dcb1fb0de
ChuNyongY <a42720653@gmail.com>
2022-11-10 22:42:28 +0900

    commit

--- WeMeet/app/src/main/java/com/example/project/MainActivity.java
+++ WeMeet/app/src/main/java/com/example/project/MainActivity.java
@@ -1,30 +1,43 @@
 package com.example.project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.icu.text.CaseMap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main_Activity";
 
-    TextView title;
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.actionbar_actions, menu) ;
-
-        return true ;
-    }
+    private ImageView WM_menu;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("WeMeet");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        //액션바 숨기기
+
+        WM_menu = findViewById(R.id.WM_menu);
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolbar);
+
+        WM_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: 클릭됨");
+                drawerLayout.openDrawer(Gravity.LEFT);
+                //클릭했을 때
+            }
+        });
     }
 }
