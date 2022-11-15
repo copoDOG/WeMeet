31c2b2fa7d1d9eb62aa4dd99e3f2ca30d091720a
ChuNyongY <a42720653@gmail.com>
2022-11-15 18:00:43 +0900

    WeMeet

--- WeMeet/app/src/main/java/com/example/project/MainActivity.java
+++ WeMeet/app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.project;
 
 import android.os.Bundle;
-import androidx.appcompat.widget.SearchView;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private ImageView WM_menu;
-    private SearchView toolbar_search;
+    private SearchView WM_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +33,37 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolbar);
         WM_menu = findViewById(R.id.WM_menu);
-        toolbar_search = findViewById(R.id.toolbar_search);
+        WM_search = findViewById(R.id.WM_search);
 
         WM_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: 클릭됨");
                 drawerLayout.openDrawer(Gravity.LEFT);
-                //클릭했을 때
+                //메뉴 버튼을 클릭했을 때
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.toolbar_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.WM_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint(getString(R.string.editText_search));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
             }
         });
     }
