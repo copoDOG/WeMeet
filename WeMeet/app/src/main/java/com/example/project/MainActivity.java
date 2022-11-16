07748560002e1e00173969533c753ce8779bfe3c
ChuNyongY <a42720653@gmail.com>
2022-11-16 15:14:53 +0900

    bottom bar

--- WeMeet/app/src/main/java/com/example/project/MainActivity.java
+++ WeMeet/app/src/main/java/com/example/project/MainActivity.java
@@ -3,12 +3,9 @@ package com.example.project;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SearchView;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -20,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private ImageView WM_menu;
-    private SearchView WM_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolbar);
         WM_menu = findViewById(R.id.WM_menu);
-        WM_search = findViewById(R.id.WM_search);
+        SearchView WM_search = findViewById(R.id.WM_search);
 
         WM_menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,29 +37,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: 클릭됨");
                 drawerLayout.openDrawer(Gravity.LEFT);
                 //메뉴 버튼을 클릭했을 때
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
-
-        getMenuInflater().inflate(R.menu.toolbar_search, menu);
-
-        MenuItem searchItem = menu.findItem(R.id.WM_search);
-        SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setQueryHint(getString(R.string.editText_search));
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
             }
         });
     }
